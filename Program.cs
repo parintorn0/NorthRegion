ab91324f7b266db7db66dd3707afa039f1442905
parintorn0 <github@alias.toparin.me>
2025-03-16 17:56:00 +0700

    Default controller change to NorthRegion

--- Program.cs
+++ Program.cs
@@ -30,7 +30,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=NorthRegion}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 
